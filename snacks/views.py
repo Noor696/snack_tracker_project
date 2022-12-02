c24a054c2ab3be7dd35ef60105c72564f95ad439
Noor696 <eng.nooralhateeb@gmail.com>
2022-12-02 10:35:00 +0000

    add model

--- snacks/views.py
+++ snacks/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView,ListView
+from .models import Snack
 
 # Create your views here.
 class HomePage(TemplateView): 
     ''' responsiple show user the templete'''
     template_name='home.html'
+
+class SnackListView(ListView): 
+    template_name='snack_list.html'
+    # give it the model we want to show
+    model = Snack # the table we want take the data from. 
